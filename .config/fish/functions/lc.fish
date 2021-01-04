2de28165646326dec2f2c58f67ba5acab346a859
Rarsa <rarsa@yahoo.com>
2021-01-03 23:57:25 -0500

    Updated the function definition

--- .config/fish/functions/lc.fish
+++ .config/fish/functions/lc.fish
@@ -1,4 +1,4 @@
 # Defined in - @ line 1
-function lc --wraps='ls -ca' --wraps='ls -Ca' --description 'alias lc=ls -Ca'
+function lc --wraps='ls -Ca' --description 'alias lc=ls -Ca'
   ls -Ca $argv;
 end
