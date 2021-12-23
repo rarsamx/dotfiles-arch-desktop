4fed2b4af36b5978d64303db61f95d7174c6977e
Rarsa <rarsa@yahoo.com>
2021-12-23 13:47:06 -0500

    =Added xmonad recompilation after an update

--- .config/fish/functions/pacupd.fish
+++ .config/fish/functions/pacupd.fish
@@ -1,4 +1,6 @@
 function pacupd -w 'sudo pacman -Syu' -d 'Pacman update with my preferred defaults'
     echo "sudo pacman -Syu" $argv 
     sudo pacman -Syu $argv
+    echo "xmonad --recompile; xmonad --restart"
+    xmonad --recompile; xmonad --restart
 end
