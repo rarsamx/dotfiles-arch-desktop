4ac5a18d9e7280cde24ab5bd5637713605cace8f
Rarsa <rarsa@yahoo.com>
2022-01-07 00:42:10 -0500

    Don't show all files

--- .config/fish/functions/ll.fish
+++ .config/fish/functions/ll.fish
@@ -1,4 +1,4 @@
 # Defined in - @ line 1
-function ll --wraps='ls -ahl' --description 'alias ll=ls -ahl'
-  ls -ahl $argv;
+function ll --wraps='ls -hl' --description 'alias ll=ls -hl'
+  ls -hl $argv;
 end
