7cab1bf718b4caaeb1f26fc620088954feb08dd2
Rarsa <rarsa@yahoo.com>
2022-03-20 05:23:57 -0400

    Added icons for xmobar

---
+++ .config/xmobar/icons/iconfiles.fish
@@ -0,0 +1,13 @@
+
+#!/usr/bin/env fish
+
+inkscape /usr/share/icons/Mint-Y/status/symbolic/status-nm-signal-00-symbolic.svg -o status-nm-signal-00.png
+convert status-nm-signal-00.png status-nm-signal-00.xpm
+inkscape /usr/share/icons/Mint-Y/status/symbolic/status-nm-signal-25-symbolic.svg -o status-nm-signal-25.png
+convert status-nm-signal-25.png status-nm-signal-25.xpm
+inkscape /usr/share/icons/Mint-Y/status/symbolic/status-nm-signal-50-symbolic.svg -o status-nm-signal-50.png
+convert status-nm-signal-50.png status-nm-signal-50.xpm
+inkscape /usr/share/icons/Mint-Y/status/symbolic/status-nm-signal-75-symbolic.svg -o status-nm-signal-75.png
+convert status-nm-signal-75.png status-nm-signal-75.xpm
+inkscape /usr/share/icons/Mint-Y/status/symbolic/status-nm-signal-100-symbolic.svg -o status-nm-signal-100.png
+convert status-nm-signal-100.png status-nm-signal-100.xpm
