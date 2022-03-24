ece415912457d433d9f61e5f489595d062ec8e8f
Rarsa <rarsa@yahoo.com>
2022-03-23 23:02:31 -0400

    Alias to ls to avoid using parameters

---
+++ .config/fish/functions/l1.fish
@@ -0,0 +1,3 @@
+function l1 --wraps='ls -1' --description 'alias l1=ls -1'
+  ls -1 $argv; 
+end
---
+++ .config/fish/functions/la1.fish
@@ -0,0 +1,3 @@
+function la1 --wraps='ls -a1' --description 'alias la1=ls -a1'
+  ls -a1 $argv; 
+end
