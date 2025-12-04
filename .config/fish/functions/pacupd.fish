051a552302cf2f01adb8709d7332a83cc2e6b835
Rarsa <rarsa@yahoo.com>
2025-12-04 17:45:15 -0500

    Check number of updates before executing and only recompile xmonad if actually updated

--- .config/fish/functions/pacupd.fish
+++ .config/fish/functions/pacupd.fish
@@ -1,6 +1,17 @@
 function pacupd -w 'sudo pacman -Syu' -d 'Pacman update with my preferred defaults'
-    echo "sudo pacman -Syu" $argv 
-    sudo pacman -Syu $argv
-    echo "xmonad --recompile; xmonad --restart"
-    xmonad --recompile; xmonad --restart
+    
+    set numUpd (checkupdates | wc -l)
+    if test $numUpd -gt 0
+        echo "sudo pacman -Syu" $argv 
+        sudo pacman -Syu $argv
+        if test $status -eq 0
+            echo "xmonad --recompile; xmonad --restart"
+            xmonad --recompile; xmonad --restart
+            read -n1 -P "Update finished. Press Enter to continue"
+        else
+            read -n1 -P "Error updating. Press Enter to continue"
+        end
+    else
+        read -n1 -P "System is up to date. Press Enter to continue"
+    end
 end
